95d12be00110ac99dae4139b637223c72f012767
slavakorotkov <slavik.pro0101@gmail.com>
2023-10-09 21:18:04 +0300

    1.0.0

---
+++ mymath.go
@@ -0,0 +1,18 @@
+package mymath1
+
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+// Другие математические функции...
